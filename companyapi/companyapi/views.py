96a80479ad9b691ab85931bd61b2236ec29c7b8b
Rushikesh Chavan <113137155+rushikeshchavan95@users.noreply.github.com>
2025-06-18 23:42:42 +0530

    Update views.py

--- companyapi/companyapi/views.py
+++ companyapi/companyapi/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 def home_page(request):
     print("Home page")
     
-    return HttpResponse("<h1>This is home page<h1>")
+    return HttpResponse("<h1>This is home page.<h1>")
